dffa4f870bbe17e6d7affb099b940dbc4aee8bac
Sujana Maruvada <sujana1207@gmail.com>
2020-11-26 16:41:43 +0530

    first_commit

---
+++ Models/JWT.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ODLClientApp.Models
+{
+    public class JWT
+    {
+
+        [JsonProperty("token")]
+        public string Token { get; set; }
+    }
+}
